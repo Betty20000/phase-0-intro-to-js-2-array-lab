d204a772a8c880e2fe899ec19da6e234b403be1b
Beatrice Gachigi <beatricegachigi@gmail.com>
2024-03-24 20:52:27 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+let cats = [ 'Milo', 'Otis', 'Garfield' ];
+
+function destructivelyPrependCat(name) {
+
+    cats.unshift(name);
+  }
+
+  destructivelyPrependCat("Tosh",cats);
+  console.log(cats);
+  
+
+
+
+
+
+
